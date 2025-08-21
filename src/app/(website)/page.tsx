c43f15bc020196612d385b6f810ed0fc460b6c36
Lucas Alvares <lucas.alvaress@hotmail.com>
2025-08-21 17:15:00 -0300

    feat: create products components and sections

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,15 +1,22 @@
 import { Banners } from "@/components/home/banners";
 import { InfoCards } from "@/components/home/infoCards";
+import { MostSoldProducts } from "@/components/home/mostSoldProducts";
+import { MostViewedProducts } from "@/components/home/mostViewedProducts";
 import { ProductListSkeleton } from "@/components/home/productListSkeleton";
 import { data } from "@/data";
+import { Suspense } from "react";
 
 export default function Page() {
     return (
         <div>
             <Banners list={data.banners} />
             <InfoCards />
-            <ProductListSkeleton />
-            <ProductListSkeleton />
+            <Suspense fallback={<ProductListSkeleton />}>
+                <MostViewedProducts />
+            </Suspense>
+            <Suspense fallback={<ProductListSkeleton />}>
+                <MostSoldProducts />
+            </Suspense>
         </div>
     );
 }
---
+++ src/components/home/mostSoldProducts.tsx
@@ -0,0 +1,18 @@
+import { data } from "@/data";
+import { ProductList } from "../products/productList";
+
+export const MostSoldProducts = () => {
+    return (
+        <div className="mt-10">
+            <h2 className="text-2xl text-center md:text-left">
+                Produtos mais vendidos
+            </h2>
+            <p className="text-gray-500 text-center md:text-left">
+                Campeões de vendas da nossa loja.
+            </p>
+            <div className="mt-9">
+                <ProductList list={data.mostSoldProducts} />
+            </div>
+        </div>
+    );
+};
